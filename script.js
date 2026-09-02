1feda6c30265fe3cf5ced3a7b7fb77cfea8e57c2
Facundo <facundo.palacio@mi.unc.edu.ar>
2026-09-01 23:39:44 -0300

    Step 2: Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
- 
-console.log("Hello World");
+
+/*
+This function uses (pseudo) random numbers transformed to integers 
+between 0 to 2 in order to get from the array the choice stored in 
+those index numbers.
+It doesn't have an input parameter and returns a string.
+*/
+
+function getComputerChoice() {
+
+const choices = ["rock","paper","scissors"];
+
+let value = Math.random() * 3;
+
+return choices[Math.floor(value)];
+}
+
+console.log(getComputerChoice());
