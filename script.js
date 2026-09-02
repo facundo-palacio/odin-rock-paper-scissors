6efbb5298e00a25590064e01054cb12638d8f531
Facundo <facundo.palacio@mi.unc.edu.ar>
2026-09-02 00:16:55 -0300

    Step 3: Write the logic to get the human choice

--- script.js
+++ script.js
@@ -15,4 +15,24 @@ let value = Math.random() * 3;
 return choices[Math.floor(value)];
 }
 
-console.log(getComputerChoice());
+/*This function allows the user to choose between rock, paper and scissors
+and returns its choice. I made the decision of (pseudo) randomly choose
+one option for it if the input is invalid. And restricted its options to 
+numbers. This will be reviewed and reformatted later.*/
+
+function getHumanChoice() {
+    let wrongInput = true;
+
+    const choice = ["rock", "paper", "scissors"];
+    
+ 	let humanChoice = prompt("Choose a number: 1 - rock, 2 - paper, 3 - scissors");
+ 	if (humanChoice == 1 | humanChoice == 2 | humanChoice == 3){
+        return choice[humanChoice-1];
+
+    }else {
+    	return choice[Math.floor(Math.random() * 3)]
+    }
+} 
+
+console.log(getHumanChoice());
+
