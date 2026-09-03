f1a552cdbff7837dd8ad7f05dd6ff1981557e8f6
Facundo <facundo.palacio@mi.unc.edu.ar>
2026-09-02 23:45:38 -0300

    Step 6: Write the logic to play the entire game

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-let humanScore = 0;
-let computerScore = 0;
-
-
 /*
 This function uses (pseudo) random numbers transformed to integers 
 between 0 to 2 in order to get from the array the choice stored in 
@@ -31,22 +27,51 @@ function getHumanChoice() {
 } 
 
 
-/*Playround has the logic of a single round. I stored the losing cases in variables for readability.
-The first if statement handles all the ties. And in all the other cases the user wins.
-*/
-function playRound(humanChoice,computerChoice) {
+function playGame(){
+    /*Playround has the logic of a single round. I stored the losing cases in variables for readability.
+    The first if statement handles all the ties. And in all the other cases the user wins.
+    */  
+    function playRound(humanChoice,computerChoice) {
 
-    let lose1 = (humanChoice == "paper" && computerChoice == "scissors");    
-    let lose2 = (humanChoice == "scissors" && computerChoice == "rock");
-    let lose3 = (humanChoice == "rock" && computerChoice == "paper");
+        let retval;
+
+        let lose1 = (humanChoice == "paper" && computerChoice == "scissors");    
+        let lose2 = (humanChoice == "scissors" && computerChoice == "rock");
+        let lose3 = (humanChoice == "rock" && computerChoice == "paper");
     
-    if (humanChoice == computerChoice){
-    	console.log("It's a tie.");
-    } else if (lose1 | lose2 | lose3){  
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-	}else {
-	    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-	}
+        if (humanChoice == computerChoice){
+    	    console.log("It's a tie.");
+    	    retval = "tie";
+        } else if (lose1 | lose2 | lose3){  
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            retval = "computer_wins";
+	    }else {
+	        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+	        retval = "human_wins";
+	    }
+	    return retval;
+    }
+
+    
+    let humanScore = 0;
+    let computerScore = 0;    
+
+    for(let i = 1; i <= 5; i++){
+    	let result = playRound(getHumanChoice(),getComputerChoice());
+    	if (result == "computer_wins"){
+    	    computerScore += 1;
+    	} else if(result == "human_wins"){
+    		humanScore += 1;
+    	}
+    }
+
+    if (computerScore > humanScore){
+    	console.log("The computer won this game. :(");
+    }else if  (humanScore < humanScore) {
+        console.log("You won this game. Congratulations! :)");	
+    }else {
+    	console.log("No one won this time. But no one lost either. :|");
+    }    	
 }
 
-playRound(getHumanChoice(),getComputerChoice());
+playGame();
